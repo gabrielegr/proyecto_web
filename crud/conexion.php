1571c476411f8d26a7a5761ce58063f001d3c3ca
gabrielegr <00199518@uca.edu.sv>
2019-11-17 00:44:03 -0600

    Fix db password

--- crud/conexion.php
+++ crud/conexion.php
@@ -1,7 +1,7 @@
 
 <?php
 
-$conn_string = "host=localhost port=5432 dbname=proyecto_web user=admin password=washiwah095 options='--client_encoding=UTF8'";
+$conn_string = "host=localhost port=5432 dbname=proyecto_web user=admin password=root options='--client_encoding=UTF8'";
 $dbconn = pg_connect($conn_string);
 if(!$dbconn) {
 echo "Error: No se ha podido conectar a la base de datos\n";
